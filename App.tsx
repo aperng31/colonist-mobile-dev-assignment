0c7ea5bf0bdfd26b5b22214b896284f4d41f2d8c
Alan Perng <aperng31@gmail.com>
2025-02-13 14:21:50 -0600

    successful axios request for data

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [userData, setUserData] = useState([])
+  
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await axios.get('https://6799ee3d747b09cdcccd06bc.mockapi.io/api/v1/users')
+      setUserData(response.data);
+    }
+
+    getUsers();
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
